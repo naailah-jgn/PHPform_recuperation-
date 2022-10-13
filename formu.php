23be37b760ee35ce9bba00ffe8b7c8b4d3e82485
naailah jugon <jugonnaailah@gmail.com>
2022-10-13 12:57:49 +0200

    securisation

--- formu.php
+++ formu.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,26 +17,30 @@
         <form action="/thanks.php" method="post">
             <div>
                 <label for="lastname">Lastname</label>
-                <input type="text" id="lastname" name="user_lastname">
+                <input type="text" id="lastname" name="user_lastname" required>
+                
             </div>
 
             <div>
                 <label for="firstname">Firstname</label>
-                <input type="text" id="firstname" name="user_firstname">
+                <input type="text" id="firstname" name="user_firstname" required>
+              
             </div>
 
             <div>
                 <label for="email">Courriel</label>
-                <input type="email" id="email" name="user_email">
+                <input type="email" id="email" name="user_email" required>
+                
             </div>
 
             <div>
                 <label for="phone number">Contact Number</label>
-                <input type="tel" id="phone" name="contact_number" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}">
+                <input type="tel" id="phone" name="contact_number" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>
+                
             </div>
 
             <div class="selection">
-                <select name="subject" id="subject">
+                <select name="user_subject" id="subject" required>
                 <option value="">--Please choose an option--</option>
                 <option value="claim">Complain</option>
                 <option value="compliment">Compliment</option>
@@ -45,6 +51,7 @@
             <div>
                 <label for="message">Message</label>
                 <textarea id="message" name="user_message"></textarea>
+               
             </div>
 
             <div>
--- thanks.php
+++ thanks.php
@@ -1,11 +1,45 @@
 <?php
 
-$lastname = $_POST['user_lastname'];
-$firstname = $_POST['user_firstname'];
-$subject = $_POST['subject'];
-$email = $_POST['user_email'];
-$contact_number = $_POST['contact_number'];
-$user_message = $_POST['user_message'];
+$user_lastname = $user_firstname = $user_email = $contact_number = $user_message = $user_subject = "";
+$user_lastnameErr = $user_firstnameErr = $user_emailErr = $contact_numberErr = $user_messageErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["user_lastname"])) {
+        $user_lastnameErr = "Lastname is required";
+    }else{
+        $user_lastname = $_POST['user_lastname'];
+    }
+
+    if (empty($_POST["user_firstname"])) {
+        $user_firstnameErr = "Firstname is required";
+    }else{
+        $user_firstname = $_POST['user_firstname'];
+    }
+
+    if (empty($_POST["user_email"])) {
+        $user_emailErr = "Email is required";
+    }else{
+        $user_email = $_POST['user_email'];
+    }
+
+    if (empty($_POST["contact_number"])) {
+        $contact_numberErr = "Phone Number is required";
+    }else{
+        $contact_number = $_POST['contact_number'];
+    }
+
+    if (empty($_POST["user_subject"])) {
+        $user_subjectErr = "Subject is required";
+    }else{
+        $user_subject = $_POST['user_subject'];
+    }
+    
+    if (empty($_POST["user_message"])) {
+        $user_messageErr = "Message is required";
+    }else{
+        $user_message = $_POST['user_message'];
+    }
+    
+}
 
 ?>
 
@@ -18,17 +52,18 @@ $user_message = $_POST['user_message'];
     <title>Page soumise</title>
 </head>
 <body>
-    <h1>Merci <?= $lastname ?> <?= $firstname ?> de nous avoir contacté à propos de “
-    <?= $subject ?>”.</h1>
+    <section class="response"> 
+    <h1>Merci <?= $user_lastname ?> <?= $user_firstname ?> de nous avoir contacté à propos de “
+    <?= $user_subject ?>”.</h1>
     <article> 
-    <p>Un de nos conseillers vous contactera soit à l’adresse <?= $email ?>
+    <p>Un de nos conseillers vous contactera soit à l’adresse <?= $user_email ?>
         ou par téléphone au <?= $contact_number ?> dans les plus brefs délais pour traiter votre demande : 
     </p>
     </article>
 
     <article>
-    <p> <?= $user_message ?></p>
+    <p><em>"<?= $user_message ?>"<em></p>
     </article>
-    
+    </section>
 </body>
 </html>
